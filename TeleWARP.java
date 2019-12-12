d152f9f961efcef9a57998ca316d9d30339bd15c
Zeb Engberg <zebengberg@gmail.com>
2019-12-12 09:52:31 -0700

    teleop platform servo work

--- TeleWARP.java
+++ TeleWARP.java
@@ -20,24 +20,20 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @TeleOp
 public class TeleWARP extends LinearOpMode {
-
     DcMotor front_left_wheel;
     DcMotor back_left_wheel;
     DcMotor back_right_wheel;
     DcMotor front_right_wheel;
+    DcMotor[] motors;
 
     Servo left_arm;
     Servo right_arm;
 
+    Servo left_platform;
+    Servo right_platform;
+    boolean platform_state;
+
     BNO055IMU imu;
-
-    // main arm motors
-    //DcMotor elbow;
-    //Servo wrist;
-    //Servo finger;
-
-
-
 
 
     @Override
@@ -47,42 +43,50 @@ public class TeleWARP extends LinearOpMode {
         front_left_wheel = hardwareMap.dcMotor.get("front_left_wheel");
         back_left_wheel = hardwareMap.dcMotor.get("back_left_wheel");
         back_right_wheel = hardwareMap.dcMotor.get("back_right_wheel");
-        // Arbitrary decision to reverse all -- could instead negate powers below.
-        front_left_wheel.setDirection(DcMotor.Direction.REVERSE);
-        back_left_wheel.setDirection(DcMotor.Direction.REVERSE);
-        front_right_wheel.setDirection(DcMotor.Direction.REVERSE);
-        back_right_wheel.setDirection(DcMotor.Direction.REVERSE);
-        front_left_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        back_left_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        front_right_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        back_right_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        // Creating an array of motors so we can iterate over it.
+        motors = new DcMotor[] {back_left_wheel, back_right_wheel, front_right_wheel, front_left_wheel};
+
+        // Initializing the motors.
+        for (DcMotor motor : motors) {
+            // REV HD Hex encoder counts 2240 per rotation.
+            motor.setDirection(DcMotor.Direction.REVERSE);
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
 
         // Servos for little arms
         left_arm = hardwareMap.servo.get("left_arm");
         right_arm = hardwareMap.servo.get("right_arm");
-        left_arm.resetDeviceConfigurationForOpMode();
-        right_arm.resetDeviceConfigurationForOpMode();
         left_arm.setDirection(Servo.Direction.FORWARD);
         right_arm.setDirection(Servo.Direction.REVERSE);
+        left_arm.setPosition(0);
+        right_arm.setPosition(0);
 
-        // Motors for big arm
-        //elbow = hardwareMap.dcMotor.get("elbow");
-        //elbow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //wrist = hardwareMap.servo.get("wrist");
-        //finger = hardwareMap.servo.get("finger");
-        //wrist.resetDeviceConfigurationForOpMode();
-        //finger.resetDeviceConfigurationForOpMode();
-        //boolean finger_state = false;
+        // Servos for moving the platform.
+        left_platform = hardwareMap.servo.get("left_platform");
+        right_platform = hardwareMap.servo.get("right_platform");
+        left_platform.setDirection(Servo.Direction.FORWARD);
+        right_platform.setDirection(Servo.Direction.REVERSE);
+        left_platform.setPosition(0);
+        right_platform.setPosition(0);
+        // Keep track of the whether the platform servos are up or down.
+        platform_state = false;
 
-        // IMU DEVICE -- possibly delete some of this.
+
+        // IMU DEVICE
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.mode                = BNO055IMU.SensorMode.IMU;
-        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.loggingEnabled      = false;
+        parameters.mode = BNO055IMU.SensorMode.GYRONLY;
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        parameters.loggingEnabled = false;
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(parameters);
+
+        // Possibly delete
+        //parameters.mode                = BNO055IMU.SensorMode.IMU;
+        //parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        //parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        //parameters.loggingEnabled      = false;
 
         // Make sure the imu gyro is calibrated before continuing.
         telemetry.addData("Status", "calibrating gyro");
@@ -145,35 +149,17 @@ public class TeleWARP extends LinearOpMode {
             }
 
 
-            /*
-            // Big arm controls
-            // TODO: Use encoders to limit movement of elbow motor.
-            if (gamepad1.dpad_down) {
-                elbow.setPower(.2);
-            } else if (gamepad1.dpad_up) {
-                elbow.setPower(-.2);
+            if (gamepad1.y) {
+                platform_state = !platform_state;
+            }
+
+            if (platform_state) {
+                left_platform.setPosition(0.4);
+                right_platform.setPosition(0.5);
             } else {
-                elbow.setPower(0);
+                left_platform.setPosition(0);
+                right_platform.setPosition(0);
             }
-
-            if (gamepad1.a) {
-                finger_state = !finger_state;
-            }
-
-            if (finger_state) {
-               finger.setPosition(1);
-            } else {
-                finger.setPosition(0);
-            }
-
-            // controlling a servo as if it were a DC motor
-            if (gamepad1.x) {
-                wrist.setPosition(Math.min(wrist.getPosition() + .1, 1));
-            } else if (gamepad1.b) {
-                wrist.setPosition(Math.max(wrist.getPosition() - .1, 0));
-            }
-
-             */
 
 
             telemetry.addData("Front Left ", front_left_wheel.getPower());
